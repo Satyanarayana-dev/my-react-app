09e89b08fae4cff77e4e364a3acf9f314cdb88af
satyam <satyam.gcs@gmail.com>
2021-04-21 20:17:03 +0530

    config

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_dpKzETnxL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '436ca6prbtf2c9mkh3vffli3qa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
